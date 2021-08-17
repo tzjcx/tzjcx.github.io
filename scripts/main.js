132ec5eb72ec4ed9aea9bbeb9f742d5e8f3b4221
tzjcx <88773433+tzjcx@users.noreply.github.com>
2021-08-17 20:05:02 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -15,19 +15,30 @@ let MyHeading = document.querySelector('h1');
 function setUserName(){
     let myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    MyHeading.textContent = 'Hello, '+ myName;
+    MyHeading.textContent = 'Hello, I\'m '+ myName;
 }
 
 if (!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    MyHeading.textContent = 'Hello, '+ storedName;
+    MyHeading.textContent = 'Hello, I\'m '+ storedName;
 }
 
 MyButton.onclick = function() {
     setUserName();
 }
+
+let colorPicker=document.getElementById("colorPicker");
+let box=document.getElementById("box");
+let output=document.getElementById("output");
+let body=document.querySelector("div#mainfo");
+
+body.style.borderColor=colorPicker.value;
+colorPicker.addEventListener("input",function(event){body.style.borderColor=event.target.value;},false);
+colorPicker.addEventListener("change",function(event){output.textContent="Color set to "+colorPicker.value;},false);
+
+
 /* document.querySelector('html').onclick = function() {
     alert('Ouch! Stop poking me!');
 } */
